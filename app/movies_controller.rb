7837900f2fbf0af3534420c3cb78200810430870
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 22:21:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,7 +74,6 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie.update(title: "Wat, huh?")
-  movie.save
 end
 
 def can_update_multiple_items_at_once
