d2752382c185a4363a25035e7a711c80676fd4d7
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 22:13:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
-  Movie.create(title,release_date) do |m|
+  Movie.create(title, release_date) do |m|
     m.title
     m.release_date
   end
